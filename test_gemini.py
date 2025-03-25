0b0ad523a592017333ae0a459dd8715d56d8e1d1
KARTHIKA-id12 <147937445+KARTHIKA-id12@users.noreply.github.com>
2025-03-25 11:35:25 +0530

    Update test_gemini.py

--- test_gemini.py
+++ test_gemini.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyDf22ITgR26v-DlL90SxfV1BHjz8Jn1NGM")
+genai.configure(api_key="-----------------------")
 
 models = genai.list_models()
 for model in models:
